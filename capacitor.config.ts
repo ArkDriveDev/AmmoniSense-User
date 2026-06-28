e04bfae58d0a39bbfc703d4534a7ae02353a2f2e
Arky <20231736@nbsc.edu.ph>
2026-06-29 07:43:13 +0800

    Updated capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,27 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Ammonisense-User',
-  webDir: 'dist'
+  appId: 'com.ammonisense.user',  
+  appName: 'Ammonisense',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    cleartext: true
+  },
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert']
+    },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: '#ffffff',
+      showSpinner: false
+    },
+    StatusBar: {
+      style: 'dark',
+      backgroundColor: '#ffffff'
+    }
+  }
 };
 
 export default config;
