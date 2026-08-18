c73e7c2f177429b3de36036dc6c1c1de423539f0
Arky <20231736@nbsc.edu.ph>
2026-08-18 13:19:30 +0800

    chore(config): add vite.config setup

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx']
+    }
+  }
+})
