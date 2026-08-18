6d6cd59fdd641ecc25e6e1aa8993f9492bb44f93
Arky <20231736@nbsc.edu.ph>
2026-08-18 13:14:42 +0800

    feat(map): add AreaMapping page for spatial polygon management (part 1)

---
+++ src/pages/user/AreaMapping.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonBadge,
+  IonButton,
+  IonIcon,
+  IonSpinner,
+  IonToast,
+  IonRefresher,
+  IonRefresherContent
+} from '@ionic/react';
