bb613b66742f144ed7a5e986bb0f929ca4bf32b2
Arky <20231736@nbsc.edu.ph>
2026-08-18 13:15:12 +0800

    fix(map): filter deleted offline sites and handle site deletion refresh (part 3)

--- src/pages/user/UserMap.tsx
+++ src/pages/user/UserMap.tsx
@@ -24,16 +24,16 @@ import {
   closeOutline,
   hardwareChipOutline,
   eyeOutline,
-  locationOutline
+  locationOutline,
+  trashOutline
 } from 'ionicons/icons';
 
 import { supabase } from '../../services/supabase';
 import offlineStorage from '../../services/OfflineStorageService';
+import { fetchOdorZones, fetchCommunityPolygons, deleteSite } from '../../services/siteService';
+import { OdorZone, CommunityPolygon } from '../../types/site';
 import FullMapView, {
   SiteMarkerData,
-  ReadingMarkerData,
-  PhotoTagMarkerData,
-  SITE_BRAND_COLOR,
   PHOTO_TAG_BRAND_COLOR,
   getAmmoniaColor,
   getAmmoniaSeverityLabel
