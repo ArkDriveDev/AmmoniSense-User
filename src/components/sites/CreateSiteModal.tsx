e6f92c0d1a3a578b846655abfc85da9a70e3132f
Arky <20231736@nbsc.edu.ph>
2026-08-18 13:19:54 +0800

    feat(sites): add CreateSiteModal component (part 1)

---
+++ src/components/sites/CreateSiteModal.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import {
+  IonModal,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonButton,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
+  IonTextarea,
+  IonSpinner,
+  IonToast,
+  IonIcon,
+  IonRow,
+  IonCol,
+  IonGrid,
+  IonCard,
+  IonCardContent,
+  IonBadge,
+  IonRange
+} from '@ionic/react';
+import {
+  locateOutline,
+  addCircleOutline,
+  cameraOutline,
+  imageOutline,
+  checkmarkCircleOutline,
+  warningOutline,
+  navigateOutline
+} from 'ionicons/icons';
