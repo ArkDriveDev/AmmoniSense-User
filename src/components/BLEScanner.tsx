ae59712d029907ad88f1a26d630f561a6ba8b92c
Arky <20231736@nbsc.edu.ph>
2026-08-18 13:15:03 +0800

    feat(ble): create BLEScanner modal component with permission status bar (part 1)

---
+++ src/components/BLEScanner.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import {
+  IonModal,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonButton,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonIcon,
+  IonBadge,
+  IonSpinner,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonAlert
+} from '@ionic/react';
+import {
