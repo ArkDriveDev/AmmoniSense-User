31abd22faf1cbf8f3c96d9286710bb16ca506ab2
Arky <20231736@nbsc.edu.ph>
2026-08-18 13:14:35 +0800

    feat(map): add PolygonDrawer component (part 1)

---
+++ src/components/map/PolygonDrawer.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useRef, useState } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import {
+  IonCard,
+  IonCardContent,
+  IonButton,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
+  IonBadge,
+  IonChip
+} from '@ionic/react';
+import {
+  shapesOutline,
+  refreshOutline,
+  checkmarkCircleOutline,
