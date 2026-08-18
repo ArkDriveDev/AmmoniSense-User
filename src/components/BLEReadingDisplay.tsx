347d0cce5398bd3dad08cd6a951f894fc5ca78e2
Arky <20231736@nbsc.edu.ph>
2026-08-18 13:15:02 +0800

    feat(ble): create BLEReadingDisplay telemetry gauge component (part 1)

---
+++ src/components/BLEReadingDisplay.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  IonCard,
+  IonCardContent,
+  IonBadge,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonIcon
+} from '@ionic/react';
+import {
+  flashOutline,
+  thermometerOutline,
+  waterOutline,
+  wifiOutline,
+  timeOutline,
+  checkmarkCircleOutline,
+  alertCircleOutline,
+  warningOutline
+} from 'ionicons/icons';
