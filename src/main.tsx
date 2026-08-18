7ff97ebc9d120317e0cc942d36bb41d927cea9c1
Arky <20231736@nbsc.edu.ph>
2026-08-18 13:19:31 +0800

    feat: add src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Initialize Capacitor PWA Elements for Camera & Web View Fallbacks
+defineCustomElements(window);
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
