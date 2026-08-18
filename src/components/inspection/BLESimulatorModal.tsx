edfe5d15db8972bed14c6f0c9236e9187f2ef6e3
Arky <20231736@nbsc.edu.ph>
2026-08-18 13:15:09 +0800

    refactor(ble): remove BLESimulatorModal component (prune part 13)

--- src/components/inspection/BLESimulatorModal.tsx
+++ src/components/inspection/BLESimulatorModal.tsx
@@ -19,23 +19,3 @@ import {
   IonCol
 } from '@ionic/react';
 import {
-  bluetoothOutline,
-  closeOutline,
-  radioOutline,
-  flashOutline,
-  alertCircleOutline,
-  checkmarkCircleOutline,
-  warningOutline,
-  refreshOutline
-} from 'ionicons/icons';
-import bleService, { BLEReading } from '../../services/bleService';
-
-interface BLESimulatorModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-  onSimulatedReading?: (reading: BLEReading) => void;
-}
-
-export const BLESimulatorModal: React.FC<BLESimulatorModalProps> = ({
-  isOpen,
-  onClose,
