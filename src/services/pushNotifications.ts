5a5b447ba31951f7813a20732a38b7ed7b99ae59
Arky <20231736@nbsc.edu.ph>
2026-08-18 13:22:32 +0800

    feat(services): add pushNotifications service module

---
+++ src/services/pushNotifications.ts
@@ -0,0 +1,3 @@
+// Push notifications disabled for web/environmental monitoring application
+export const initializePushNotifications = async () => {};
+export const removeDeviceToken = async () => {};
