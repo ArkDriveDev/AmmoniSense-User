95c8fae674c821096bf40482abd18ab06389e4ab
JethroBalaga <balagajet@gmail.com>
2026-08-31 15:49:04 +0800

    fix(android): register BluetoothLowEnergyPlugin in MainActivity and add BLUETOOTH_ADVERTISE

--- android/app/src/main/java/com/ammonisense/user/MainActivity.java
+++ android/app/src/main/java/com/ammonisense/user/MainActivity.java
@@ -1,5 +1,13 @@
 package com.ammonisense.user;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import ee.forgr.plugin.bluetooth_low_energy.BluetoothLowEnergyPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(BluetoothLowEnergyPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
